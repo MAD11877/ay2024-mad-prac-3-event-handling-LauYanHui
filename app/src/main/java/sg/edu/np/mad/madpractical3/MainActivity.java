9c61c46899c8f458393223f8da5562d1cc439c8d
LauYanHui <S10241639@connect.np.edu.sg>
2024-04-28 16:13:04 +0800

    edited

--- app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -1,6 +1,9 @@
 package sg.edu.np.mad.madpractical3;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        User user = new User("John Doe","MAD Developer",1,false);
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
+
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
